2f6b4361c0bc0268d2aef2062323236e33041e6e
Mojtabakordabadi <21019204+Mojtabakordabadi@users.noreply.github.com>
2025-04-16 20:14:13 +0330

    Add files via upload

---
+++ session4/test2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, num, is_prime;
+    cout<<"enter number:"<<endl;
+    cin>>num;
+    is_prime=1;
+    for(i=2;i<=num/2;i++)
+    {
+        if (!(num%i)) 
+        is_prime=0;
+    }
+    if(is_prime)
+    cout<<"The number is prime";
+    else
+    cout<<"The number is not prime";
+     return 0;
+}
---
+++ session4/test5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int k=1;k<=5;k++)
+  {
+    for(int i=1;i<=10;i++)
+   {
+    cout<<"*";
+   } 
+  cout<<endl;
+  }
+   
+   return 0;
+}
