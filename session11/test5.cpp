c8fcdb46d34f57e5acbaa96427d90a3e506278d0
Mojtabakordabadi <21019204+Mojtabakordabadi@users.noreply.github.com>
2025-05-14 11:21:25 +0330

    Add files via upload

---
+++ session11/test5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> numbers;
+
+    
+    cout << "enter 3 numbers   :" << endl;
+    for (int i = 0; i < 3; i++) {
+        int x;
+        cin >> x;
+        numbers.push_back(x);
+    }
+
+    cout << "size : " << numbers.size() << endl;
+    cout << " items: ";
+    for (int i = 0; i < numbers.size(); i++) {
+        cout << numbers.at(i) << " ";
+    }
+    cout << endl;
+    for (int i:numbers) cout<<i<<" ";
+    cout<<endl;
+
+    numbers.insert(numbers.begin(),100);
+    cout << "after add 100 to begin ";
+    for (int x : numbers) cout << x << " ";
+    cout << endl;
+
+   
+    numbers.erase(numbers.begin() + 1);
+    cout << "after delete secon item ";
+    for (int x : numbers) cout << x << " ";
+    cout << endl;
+
+    cout << "first item : " << numbers.front() << endl;
+    cout << " last item: " << numbers.back() << endl;
+
+     
+    numbers.clear();
+    cout << "  vector size after Clear  : " << numbers.size() << endl;
+
+    return 0;
+}
