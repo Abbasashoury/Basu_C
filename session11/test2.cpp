f13b3dd5a97a60fd36a3d899d6c2848b590ed79e
Mojtabakordabadi <21019204+Mojtabakordabadi@users.noreply.github.com>
2025-05-14 11:15:09 +0330

    Add files via upload

---
+++ session11/test2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void get(int*& a,int& n)
+{
+    cout<<"Enter number of item:";
+    cin>>n;
+    a=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+}
+void print(int *a,int n)
+{
+    for(int i=0;i<n;i++)
+    cout<<a[i]<<" ";
+}
+
+int main()
+{
+   int n;
+   int *a;
+   get(a,n);
+   print(a,n);
+   
+   delete a;
+   print(a,n);
+   return 0;
+}
