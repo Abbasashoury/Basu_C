8e3eeae244621cf90527c76da8f3497912367966
Mojtabakordabadi <21019204+Mojtabakordabadi@users.noreply.github.com>
2025-05-28 09:54:32 +0330

    Add files via upload

---
+++ session12/test13.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+int main()
+{
+    string input,reverse,lower;
+    getline(cin,input);
+    cout<<"your name: "<<input<<endl;
+    for(char c:input) 
+    {
+        lower+=tolower(c);
+    }
+    //cout<<sizeof(input)<<endl;
+    for(int i=lower.length()-1;i>=0;i--)
+    reverse+=lower[i];
+    cout<<"Reverse your name: "<<reverse<<endl;
+
+    if(lower==reverse)
+    {
+        cout<< "is palindroom";
+    }
+    else{
+         cout<< "Not palindroom";
+    }
+   
+}
